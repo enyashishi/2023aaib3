d6895d4f335beca46ef20818e0cb367b729dddc7
enyashishi <145088395+enyashishi@users.noreply.github.com>
2025-01-08 10:49:32 +0800

    week18

---
+++ week18/week18-1.cpp
@@ -0,0 +1,15 @@
+//week18-1.cpp
+//LeetCode 896
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int up = 0,down =0;
+        for(int i=0;i<nums.size()-1;i++){
+            int d = nums[i+1] - nums[i];
+            if(d>0) up = 1;
+            if(d<0) down = 1;
+        }
+        if(up==1 && down==1) return false;
+        else return true;
+    }
+};
